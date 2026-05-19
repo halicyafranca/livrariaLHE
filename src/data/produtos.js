e33ad77b2daed2dc7bf2fcd5ee677302a9db2718
halicyafranca <halicyagabrielly@gmail.com>
2026-05-19 09:16:29 -0300

    FEAT: criação data e components

---
+++ src/data/produtos.js
@@ -0,0 +1,14 @@
+const livros = [
+     {titulo: "O Alienista", id:1, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+     {titulo: "O Alienista", id:2, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+      {titulo: "O Alienista", id:3, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+       {titulo: "O Alienista", id:4, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+        {titulo: "O Alienista", id:5, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+         {titulo: "O Alienista", id:6, preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+        {titulo: "O Alienista", id:7,preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+         {titulo: "O Alienista", id:8,preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+          {titulo: "O Alienista", id:9,preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+           {titulo: "O Alienista", id:10,preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"},
+            {titulo: "O Alienista", id:11,preco: 120 ,categoria:'Clássico', imagem:"./public/images/OAlienista.png"}
+]
+export {livros as listaLivros}
